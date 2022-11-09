09910505599305a045d7923a7e242bd1c404291b
satinder <satindersingh472@gmail.com>
2022-11-09 16:21:09 -0700

    finished_login_page_with_vuetify

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import EditPicture from '@/components/editPicture.vue'
-import DataShow from "@/components/dataShow.vue"
-
+import HomePage from "@/views/homePage.vue"
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/edit_picture',
-    component: EditPicture
-
-  },
-  {
     path: '/',
-    component: DataShow
+    component: HomePage,
+    meta:[
+      {
+        title: "MyRide.ml"
+      },
+      {
+        name: "author",
+        content: "Satinder Singh"
+      }
+    ]
+
   }
   
 ]
@@ -22,6 +25,36 @@ const routes = [
 const router = new VueRouter({
   routes
 })
+/*remove the old meta tag */
+router.beforeEach((to, from, next) => {
+  let current_meta_tags = document.querySelectorAll(`meta`);
+  for (let i = 0; i < current_meta_tags.length; i++) {
+    current_meta_tags[i].remove();
+  }
+  /*put the title tag inside head */
+  let new_meta_tags = to[`meta`];
+  document.querySelector(`title`)[`innerText`] = new_meta_tags[0][`title`];
+/*put the other meta tags other than the title tag inside the head */
+  for (let i = 1; i < new_meta_tags.length; i++) {
+    document
+      .querySelector(`head`)
+      .insertAdjacentHTML(
+        `afterbegin`,
+        `<meta name="${new_meta_tags[i][`name`]}" content="${
+          new_meta_tags[i][`content`]
+        }"/>`
+      );
+  }
+  document.querySelector(`head`).insertAdjacentHTML(
+    `afterbegin`,
+    `<meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width,initial-scale=1.0">`
+  );
+
+  from;
+  next();
+});
 
 
 
---
+++ src/views/homePage.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="page">
+    <v-app>
+      <nav-bar></nav-bar>
+      <v-container  class="grey lighten-3" fill-height fluid>
+        <v-layout row wrap justify-center align-center>
+          <v-flex xs12 md6 lg4 text-start>
+            <v-card-text class="font-weight-bold text-h1  yellow darken-3 " >MyRide.</v-card-text>
+            <h2 class="text-h4" >Connecting people and Cities together.</h2>
+          </v-flex>
+          <v-flex xs10 md6 lg4 text-center>
+            <v-card width="400" class="mx-auto pa-3 rounded-lg"  elevation="9">
+                <div class="yellow darken-3 rounded-lg " style="height:50px;">
+                    <v-text class="text-h4 font-weight-bold" >Login</v-text>
+                </div>
+              <v-text-field
+                class="pa-2 text-h5"
+                outlined
+                v-model="email"
+                label="Email"
+                :rules="rules"
+                hide-details="auto"
+              ></v-text-field>
+              <v-text-field
+                class="pa-2 text-h5"
+                outlined
+                v-model="password"
+                label="Password"
+                :rules="rules"
+                hide-details="auto"
+                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                 :type="show ? 'text' : 'password'"
+                 @click:append="show = !show"
+
+              ></v-text-field>
+                 <v-btn class="my-4" large color="success" dark>
+              Login
+            </v-btn>
+            <hr>
+            <v-btn class="primary my-4 "  > Create an account</v-btn>
+            </v-card>
+         
+          </v-flex>
+        </v-layout>
+      </v-container>
+
+      <div class="text-center ma-2"></div>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/navBar.vue'
+export default {
+  components: {
+    NavBar,
+  },
+    methods: {
+        send_request() {
+            
+        }
+    },
+
+
+
+  data() {
+    return {
+      rules: [(value) => !!value || 'Required.'],
+      show: false
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+// * {
+//   padding: 0px;
+//   margin: 0px;
+// }
+// .login_page {
+//   display: grid;
+//   place-items: center;
+//   text-align: center;
+//   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+// }
+// .message {
+//   display: grid;
+//   align-items: center;
+// }
+</style>
