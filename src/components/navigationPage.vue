0a9a78efc55eabb7f9a8daf0e4f306e199562678
satinder <satindersingh472@gmail.com>
2022-11-11 22:32:36 -0700

    worked_on_commenting_and_fixing_navigation

--- src/components/navigationPage.vue
+++ src/components/navigationPage.vue
@@ -1,21 +1,28 @@
 <template>
   <div>
+    <!-- this component will act as notification panel for pages
+    and it will send two api requests one for image in the corner and the other for the name
+    in the notification panel -->
     <v-app-bar app clipped-left>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-app-title class="text-h4 font-weight-bold yellow--text text--darken-3">
         MyRide
       </v-app-title>
       <v-spacer />
-      <v-list-item-avatar size="50" class="mr-6 ">
-    <v-img :src="image"></v-img>
+      <v-list-item-avatar size="50" class="mr-6">
+        <v-img :src="image"></v-img>
+        <p v-if="message_two !== undefined" >{{message_two}}</p>
       </v-list-item-avatar>
     </v-app-bar>
-
+    <!-- this is the side notification for the page in which various navigations are attached -->
     <v-navigation-drawer app v-model="drawer" clipped left>
       <v-list class="text-h6">
         <v-list-item-group>
           <v-row justify="center">
-            <h3 class="my-6">{{ details['first_name']}} {{details['last_name']}}</h3>
+            <h3 v-if="message !== undefined">name not available</h3>
+            <h3 v-if="message === undefined" class="my-6">
+              {{ details['first_name'] }} {{ details['last_name'] }}
+            </h3>
           </v-row>
           <v-divider />
           <v-list-item>
@@ -98,6 +105,7 @@
 import axios from 'axios'
 import cookies from 'vue-cookies'
 export default {
+  // on mount it will execute the two functions to send the request and get the information about the profile
   mounted() {
     this.get_profile()
     this.get_only_image()
@@ -109,13 +117,13 @@ export default {
         .request({
           url: `${process.env.VUE_APP_BASE_DOMAIN}/api/client_image`,
           responseType: 'blob',
-          headers:{
-            token: cookies.get('token')
-          }
+          headers: {
+            token: cookies.get('token'),
+          },
         })
+        // will create the url from the string that was in the response
         .then((response) => {
           this.image = URL.createObjectURL(response['data'])
-          
         })
         .catch((error) => {
           this.message_two = error['response']['data']
@@ -146,7 +154,7 @@ export default {
       details: undefined,
       message: undefined,
       message_two: undefined,
-      image: undefined
+      image: undefined,
     }
   },
 }
--- src/components/searchRides.vue
+++ src/components/searchRides.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
        <v-sheet style="display:grid;place-items:center;" min-height="300" >
-            <p class=" text-h3 font-weight-bold purple--text text--darken-3">where do you want to go?</p>
+            <p class=" text-h3 font-weight-bold purple--text text--darken-3">Where do you want to go?</p>
             <div>
               <v-text-field v-model="from_city" style="width:300px;" class="rounded-xl" outlined label="From" ></v-text-field>
               <v-text-field class="rounded-xl" outlined label="To" style="width:300px;" ></v-text-field>
