05372116080ae4a379de450b320fee776ffb6aad
satinder <satindersingh472@gmail.com>
2022-11-14 16:42:35 -0700

    working_booking_rider_patch

--- src/components/patchBookingRider.vue
+++ src/components/patchBookingRider.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
+    <!-- this component will help show the status as button if confirmed,not-confirmed, completed or not -->
     <v-row class="mr-2 my-3" justify="end">
+      <!-- this button is attached to an event and send the request to an api
+      this button only exists if booking is not yet confirmed if booking is confirmed then this -->
       <v-btn
       v-show="not_confirmed"
         class="warning"
@@ -8,14 +11,15 @@
       >
         confirm Booking
       </v-btn>
-
+<!-- this button will be shown  when booking is confirmed-->
       <v-btn v-show="confirmed" class="success">
         Booking Confirmed
       </v-btn>
     </v-row>
     <v-row justify="end" class="mr-2 my-3">
+      <!-- badge button will be shown when not confirmed and not complete -->
       <v-badge
-        v-show="not_confirmed"
+        v-show="confirmed_incomplete"
         color="error"
         icon="mdi-lock"
         overlap
@@ -24,18 +28,22 @@
           Complete Booking
         </v-btn>
       </v-badge>
+      <!-- this button will be shown when booking can be complete means when it is confirmed but not complete yet -->
       <v-btn
         v-show="not_completed"
         class="warning"
+        @click="complete_booking"
       >
         Complete Booking
       </v-btn>
+      <!-- this is shown when booking is complete -->
       <v-btn
         v-show="completed"
         class="success"
       >
         Booking Completed
       </v-btn>
+      <!-- if any error occurs during api request -->
       <h3 v-if="message !== undefined" >{{message}}</h3>
     </v-row>
   </div>
@@ -49,21 +57,24 @@ export default {
     detail: Object,
   },
   mounted () {
-    if(this.detail['is_confirmed'] === 0){
-      this.confirmed = false
-      this.not_confirmed = true
-    } else if (this.detail['is_confirmed'] === 1){
+
+    // if booking is confirmed and completed then confirmed and complete will be true
+    // this will show the green buttons for confirm and complete
+    if(this.detail['is_confirmed'] === 1 && this.detail['is_completed'] === 1){
       this.confirmed = true
-      this.not_confirmed = false
+      this.completed = true
+    // if booking confirmed but not completed then confirmed is true and not completed is true
+    // this will status green for confirmed but complete button booking will be available to make a request
+    } else if(this.detail['is_confirmed'] === 1 && this.detail['is_completed'] === 0){
+      this.confirmed = true
+      this.not_completed = true
+    // if booking not confirmed and not completed then not confirmed will be orange and can make request to confirm
+    // and complete button will be disabled so that it will activate when booking is confirmed
+    } else if(this.detail['is_confirmed'] === 0 && this.detail['is_completed'] === 0){
+      this.not_confirmed = true
+      this.confirmed_incomplete = true
     }
 
-    if(this.detail['is_completed'] === 0){
-      this.completed = false
-      this.not_completed = true
-    } else {
-      this.complete = true
-      this.not_completed = false
-    }
   },
 
   methods: {
@@ -84,6 +95,8 @@ export default {
           if(response['data']['is_confirmed'] === 1){
             this.confirmed = true
             this.not_confirmed = false
+            this.confirmed_incomplete = false
+            this.not_completed = true
           }
         })
         .catch((error) => {
@@ -107,6 +120,7 @@ export default {
           if(response['data']['is_completed'] === 1){
             this.completed = true
             this.not_completed = false
+            this.confirmed_incomplete = false
           }
         })
         .catch((error) => {
@@ -117,9 +131,10 @@ export default {
   data() {
     return {
       confirmed: false,
-      not_confirmed: true,
+      not_confirmed: false,
       completed: false,
-      not_completed: true,
+      not_completed: false,
+      confirmed_incomplete: false,
       message: undefined
     }
   },
